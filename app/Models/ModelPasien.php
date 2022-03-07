e5941278d60b658e9839e417ed2b5eaaee87c820
mfajarna <invasionfajar@gmail.com>
2022-03-08 03:27:50 +0700

    Add antrian

--- app/Models/ModelPasien.php
+++ app/Models/ModelPasien.php
@@ -85,7 +85,7 @@ class ModelPasien extends Authenticatable
 
     public function verifikasiKtp()
     {
-        return $this->hasOne(ModelStatusVerifikasiKtp::class);
+        return $this->hasOne(ModelStatusVerifikasiKtp::class, 'id');
     }
 
 
