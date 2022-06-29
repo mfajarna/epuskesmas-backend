6fa84b8e3949ab838a4650c00190d536289a5df9
mfajarna <invasionfajar@gmail.com>
2022-06-30 05:11:22 +0700

    adding surat rujukan

--- resources/views/includes/auth/script.blade.php
+++ resources/views/includes/auth/script.blade.php
@@ -1,11 +1,11 @@
         <!-- JAVASCRIPT -->
-        <script src="{{ asset('/assets/libs/jquery/jquery.min.js')}}"></script>
-        <script src="{{ asset('/assets/libs/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-        <script src="{{ asset('/assets/libs/metismenu/metisMenu.min.js')}}"></script>
-        <script src="{{ asset('/assets/libs/simplebar/simplebar.min.js')}}"></script>
-        <script src="{{ asset('/assets/libs/node-waves/waves.min.js')}}"></script>
-        <script src="{{ asset('/assets/libs/feather-icons/feather.min.js')}}"></script>
+        <script src="{{ url('/assets/libs/jquery/jquery.min.js')}}"></script>
+        <script src="{{ url('/assets/libs/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+        <script src="{{ url('/assets/libs/metismenu/metisMenu.min.js')}}"></script>
+        <script src="{{ url('/assets/libs/simplebar/simplebar.min.js')}}"></script>
+        <script src="{{ url('/assets/libs/node-waves/waves.min.js')}}"></script>
+        <script src="{{ url('/assets/libs/feather-icons/feather.min.js')}}"></script>
         <!-- pace js -->
-        <script src="{{ asset('/assets/libs/pace-js/pace.min.j')}}s"></script>
+        <script src="{{ url('/assets/libs/pace-js/pace.min.j')}}s"></script>
         <!-- password addon init -->
-        <script src="{{ asset('/assets/js/pages/pass-addon.init.js')}}"></script>
+        <script src="{{ url('/assets/js/pages/pass-addon.init.js')}}"></script>
